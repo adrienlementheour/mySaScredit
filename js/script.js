a51e6f5bb38ca93dc712ac8d505d6fbc5b46a8e1
ElisabethStereosuper <elisabeth@Chiot.local>
2014-12-08 09:52:11 +0100

    First

---
+++ js/script.js
@@ -0,0 +1,67 @@
+///////////////
+// variables //
+///////////////
+
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 50) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function() { that.tick(); }, delta);
+};
+
+function animTxt(){
+
+    var txt = $('#typewrite');
+    for (var i=0; i<txt.length; i++) {
+        var toRotate = txt[i].getAttribute('data-type');
+        var period = txt[i].getAttribute('data-period');
+           
+        if (toRotate) {
+            new TxtType(txt[i], jQuery.parseJSON(toRotate), period);
+        }
+    }
+
+}
+
+
+$(function(){
+
+    animTxt();
+
+});
+
+$( window ).resize(function() {
+
+});
