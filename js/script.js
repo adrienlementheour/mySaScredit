9117b43b682b4064be12570621ebd9b5c4836b36
ElisabethStereosuper <elisabeth@Chiot.local>
2014-12-17 10:49:43 +0100

    Page d'accueil

--- js/script.js
+++ js/script.js
@@ -1,16 +1,35 @@
-///////////////
-// variables //
-///////////////
+var rand, newImg, 
+    div = 0, 
+    imgVisible = [], 
+    img = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+/**** Animation du menu responsive ****/
+
+function setBurgerMenu(){
+    if( $(window).width() < 980 ){
+
+        $('#burger').click(function(){
+            $('#menu').toggleClass('down');
+            $('#logo').toggleClass('down');
+            setTimeout(function(){ $('#burger').toggleClass('down'); }, 300);
+            return false;
+        });
+
+    }
+}
+
+/**** Animation du texte en page d'accueil ****/
 
 var TxtType = function(el, toRotate, period) {
-        this.toRotate = toRotate;
-        this.el = el;
-        this.loopNum = 0;
-        this.period = parseInt(period, 50) || 2000;
-        this.txt = '';
-        this.tick();
-        this.isDeleting = false;
-    };
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 50) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
 
 TxtType.prototype.tick = function() {
     var i = this.loopNum % this.toRotate.length;
@@ -42,7 +61,6 @@ TxtType.prototype.tick = function() {
 };
 
 function animTxt(){
-
     var txt = $('#typewrite');
     for (var i=0; i<txt.length; i++) {
         var toRotate = txt[i].getAttribute('data-type');
@@ -52,16 +70,69 @@ function animTxt(){
             new TxtType(txt[i], jQuery.parseJSON(toRotate), period);
         }
     }
-
 }
 
 
-$(function(){
 
-    animTxt();
+/**** Animation de la mosaique en page d'accueil ****/
+
+function animMosaique(){
+    
+    function animImg(div){
+
+        var divImg = $('#mosaique').find('div'), 
+            img1 = divImg.eq(div).find('.img1'), 
+            img2 = divImg.eq(div).find('.img2');
+
+        for (var i=0; i<4; i++){
+            imgVisible[i] = divImg.eq(i).find('.up').data('img');
+        }
+
+
+        function setNewImg(){
+            rand = Math.floor(Math.random()*9);
+            for(var j in imgVisible){
+                imgVisible[j] !== img[rand] ? newImg = img[rand] : setNewImg();
+            }
+        }
+        setNewImg(); 
+
+
+        function fadeAndReplace(i1, i2){
+            i1.data('img', newImg).attr('src', 'layoutImg/home/mos' + newImg + '.jpg').addClass('up').fadeTo(1000, 1);
+            i2.removeClass('up').fadeTo(1000, 0);
+        }   
+        img1.hasClass('up') ? fadeAndReplace(img2, img1) : fadeAndReplace(img1, img2);  
+    
+
+        setTimeout(function(){
+            div === 3 ? div = 0 : div ++ ;
+            animImg(div);
+        }, 2000);
+    }
+
+    for(var x=0; x<8; x++){ 
+        $('#mosaique').find('img').eq(x).data('img', x+1); 
+    }
+    animImg(div);    
+}
+
+
+
+
+$(function(){
+    
+    setBurgerMenu();
+
+    if($('body').hasClass('home')){ 
+        animTxt(); 
+        animMosaique();
+    }
 
 });
 
-$( window ).resize(function() {
+$(window).resize(function() {
+
+    setBurgerMenu();
 
 });
